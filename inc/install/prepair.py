2ff688b1b0b8aede392eb646795c71665e1f2d5e
Andreas Eriksson <mail@andreaser.eu>
2015-10-15 00:23:10 +0200

    recreated

---
+++ inc/install/prepair.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import inc.functions
+
+printMessage = inc.functions.lines.message
+printQuestion = inc.functions.lines.question
+printExclamation = inc.functions.lines.exclamation
+
+def run():
+    os.system("clear")
+    input(printMessage("Press [ENTER] to setup the encryption "))
+    print(printMessage("Creating 'header.img'"))
+    #os.system('truncate -s 2M header.img > /dev/null')
+    disk1 = input(printQuestion("Specify your system disk. (/dev/sdx) : "))
+    print(printMessage("Using " + disk1 + " as system disk."))
+    disk2 = input(printQuestion("Specify your USB stick. (/dev/sdx) : "))
+    print(printMessage("Using " + disk2 + " as USB stick."))
+    print(printMessage("Running luksFormat with 'header.img'"))
+    #os.system('cryptsetup luksFormat " + disk1 + " --header header.img > /dev/null')
+    print(printMessage("Unlocking '/dev/sda'"))
+    #os.system('cryptsetup open --header header.img --type luks " + disk1 + " enc > /dev/null')
+    #os.system('pvcreate /dev/mapper/enc > /dev/null')
+    #os.system('vgcreate store /dev/mapper/enc > /dev/null')
+    print(printMessage("Please specify the size of your logical volumes"))
+    root = input(printQuestion("Set the size of your root volume [G] : "))
+    root = input(printQuestion("Set the size of your swap volume [G] : "))
+    #os.system("lvcreate -L " + root + " store -n root > /dev/null")
+    #os.system("lvcreate -L " + swap + " store -n swap > /dev/null")
+    print(printExclamation("Your home volume will use all remaining space [+100%FREE]"))
+    #os.system("lvcreate -l +100%FREE store -n home > /dev/null")
+    print(printExclamation("Logical volumes created!"))
+
+    input(printMessage("Press [ENTER] to build the file system and mount the volumes "))
+
+    #os.system("mkfs.ext4 /dev/store/root > /dev/null")
+    #os.system("mkfs.ext4 /dev/store/home > /dev/null")
+    #os.system("mount /dev/store/root /mnt > /dev/null")
+    #os.system("mkdir /mnt/home > /dev/null")
+    #os.sustem("mount /dev/store/home /mnt/home > /dev/null")
+    #os.system("mkswap /dev/store/swap > /dev/null")
+    #os.system("("swapon /dev/store/swap > /dev/null")
+    #os.system("mkfs.ext2 /dev/sdb1 > /den/null")
+    #os.system("mkdir /mnt/boot > /dev/null")
+    #os.system("mount /dev/sdb1 /mnt/boot > /dev/null")
+    #os.system("mv header.img /mnt/boot > /dev/null")
+
+    input(printMessage("Press [ENTER] to install the base system "))
+    #os.system("pacstrap -i /mnt base base-devel")
+    os.system("clear")
+    #os.system("genfstab -U -p /mnt >> /mnt/etc/fstab > /dev/null")
+    #os.system("cp -r files /mnt/")
+    input(printMessage("You have now prepair your arch linux installation. Press [enter] to chroot "))
+    print(printMessage("Running arch-chroot"))
+    #os.system("arch-chroot /mnt /bin/bash")
--- main.py
+++ main.py
@@ -1,37 +1,30 @@
 import sys
-import inc.color
-from inc.variables import *
-import inc.installation.prepair
-import inc.installation.install
+import os
+import inc.functions
+import inc.install.prepair
 
-print(print_line)
-print(print_logo)
-print(print_line)
+printMessage = inc.functions.lines.message
+printQuestion = inc.functions.lines.question
+printExclamation = inc.functions.lines.exclamation
 
-def intro():
-    print(print_question("What do you want to do?\n"))
-    print(print_green("1 : Prepair your system disk and USB stick\n".rjust(46)))
-    print("2 : Edit files and start configuring your system\n".rjust(52))
-    print("3 : Install packages from your self-made list\n".rjust(49))
-    install = input(print_white(" > "))
-    if install == "1":
-# ----------------------------------------------------------------------------------------------------
-        inc.installation.prepair.run()
-        sys.exit()
-# ----------------------------------------------------------------------------------------------------
-    if install == "2":
-# ----------------------------------------------------------------------------------------------------
-        inc.installation.install.run()
-        sys.exit()
-# ----------------------------------------------------------------------------------------------------
-    if install == "3":
-# ----------------------------------------------------------------------------------------------------
-        print("3")
-        sys.exit()
-# ----------------------------------------------------------------------------------------------------
+def run():
+    disks = input(printExclamation("Don't forget to setup your disks! Is this done? [Y/N] : "))
+    if disks == "N" or disks == "n":
+        print(printMessage("Please go back and setup your disks before running this script. (Use cfdisk)\n\n"))
+    if disks == "Y" or disks == "y":
+        print(printQuestion("What do you want to do?"))
+        print(printMessage("1 : Prepair your system disk and USB stick"))
+        print(printMessage("2 : Edit files and start configuring your system (not available yet)"))
+        choice = input(printQuestion(": "))
+        if choice == "1":
+            inc.install.prepair.run()
+            sys.exit()
+        if choice == "2":
+            sys.exit()
+        else:
+            print(printMessage("I did not understand that. Try again!"))
+            run()
     else:
-# ----------------------------------------------------------------------------------------------------
-        print("error")
-        sys.exit()
-# ----------------------------------------------------------------------------------------------------
-intro()
+        print(printMessage("I did not understand that. Try again!"))
+        run()
+run()
