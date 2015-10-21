3279aa35f828c6c057553a8be2637a8015689cb8
Andreas Eriksson <mail@andreaser.eu>
2015-10-21 19:05:12 +0200

    added last installation process

--- test/main.py
+++ test/main.py
@@ -27,19 +27,33 @@ base_print = system_output.print_cmd('base')
 edit_files_count = system_output.count('edit_files')
 edit_files_print = system_output.print_cmd('edit_files')
 # ---------------------------------- #
-input(print_line.message("Press [ENTER] to setup the encryption "))
-for x in range(0,encrypt_count):
-    print(encrypt_print[x])
-# ---------------------------------- #
-input(print_line.message("Press [ENTER] to setup the filesystem "))
-for x in range(0,filesystem_count):
-    print(filesystem_print[x])
-# ---------------------------------- #
-input(print_line.message("Press [ENTER] to setup the base system "))
-for x in range(0,base_count):
-    print(base_print[x])
-# ---------------------------------- #
-input(print_line.message("Press [ENTER] to setup the files"))
-for x in range(0,edit_files_count):
-    print(edit_files_print[x])
+print(print_line.message("1: Setup the encryption"))
+print(print_line.message("2: Setup the filesystem"))
+print(print_line.message("3: Setup the base system"))
+print(print_line.message("4: Move files"))
+choise = input(print_line.message("What do you want to do?"))
 
+if choise == 1 or choise == "1":
+    input(print_line.message("Press [ENTER] to setup the encryption "))
+    for x in range(0,encrypt_count):
+        print(encrypt_print[x])
+    sys.exit()
+# ---------------------------------- #
+elif choise == 2 or choise == "2":
+    input(print_line.message("Press [ENTER] to setup the filesystem "))
+    for x in range(0,filesystem_count):
+        print(filesystem_print[x])
+    sys.exit()
+# ---------------------------------- #
+elif choise == 3 or choise == "3":
+    input(print_line.message("Press [ENTER] to setup the base system "))
+    for x in range(0,base_count):
+        print(base_print[x])
+    sys.exit()
+# ---------------------------------- #
+elif choise == 4 or choise == "4":
+    input(print_line.message("Press [ENTER] to setup the files"))
+    for x in range(0,edit_files_count):
+        print(edit_files_print[x])
+    sys.exit()
+input(print_line.message("Press [ENTER] to exit"))
